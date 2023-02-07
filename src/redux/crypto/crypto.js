2754ee0fd353be994f148669ed20e0e306565021
Teklay <teklay@gmail.com>
2023-02-07 11:30:07 +0300

    Add reducer

---
+++ src/redux/crypto/crypto.js
@@ -0,0 +1,32 @@
+// Action Types
+const FETCH_CRYPTO_DATA = 'react-capstone-project/crypto/FETCH_CRYPTO_DATA';
+
+// Action Creators
+
+const fetchCryptoData = (data) => ({
+  type: FETCH_CRYPTO_DATA,
+  payload: data,
+});
+
+// Thunk Creators
+const fetchCryptoDataThunk = () => async (dispatch) => {
+  const response = await fetch('https://api.coincap.io/v2/assets');
+  const data = await response.json();
+  dispatch(fetchCryptoData(data.data));
+};
+
+// Reducer
+const initialState = [];
+
+const cryptoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CRYPTO_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+// Export
+export { fetchCryptoDataThunk };
+export default cryptoReducer;
